ebe12d4aeb45bc4d5998c269622f8fb341df4a2d
YangYoungHwan00 <qjddldi17@gmail.com>
2023-12-16 21:59:48 +0900

    add click event

--- mydjango/e_manager/static/javascript/main.js
+++ mydjango/e_manager/static/javascript/main.js
@@ -1,6 +1,12 @@
 function tuple_select(){
     const $tuple = document.getElementsByClassName("table-tuple");
-    
+    for(let i=0; i<$tuple.length; i++)
+    {
+        $tuple[i].onclick() = function(){
+            return function(){
+                this.style.backgroundColor="blue";
+            }
+        }
+    }
     //$tuple[2].style.backgroundColor = "black";
 }
-
