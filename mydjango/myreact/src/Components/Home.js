2fbc8baf7d12eecd21eea6c7b95001fc542929cf
YangYoungHwan00 <qjddldi17@gmail.com>
2024-01-06 23:16:07 +0900

    add info delete button

--- mydjango/myreact/src/Components/Home.js
+++ mydjango/myreact/src/Components/Home.js
@@ -13,7 +13,7 @@ function Home(){
   }
 
   function clearAllInputs(e){
-    var allInputs = document.querySelectorAll('input');
+    var allInputs = document.querySelectorAll('.e_info_input');
     allInputs.forEach(singleInput => singleInput.value = '');
   }
 
@@ -57,13 +57,23 @@ function Home(){
     clearAllInputs();
   }
 
+  function delBtnHandler(e){
+    var del_btn = document.getElementById('del_btn');
+    const e_id = del_btn.value;
+    const api_url = 'http://127.0.0.1:8000/api/id=';
+    fetch(api_url + e_id,{
+      method: 'DELETE',
+    })
+  } 
+
   array = emp;
     return(
       <div id='Home'>
         <nav>
           <Link to='/Another'>main</Link><br/>
         </nav>
-        <input type='button' value='delete row' />
+        <input id='del_btn' type='input'></input>
+        <input type='button' value='delete row' onClick={delBtnHandler}/>
 
         <h1><p>E Manager</p></h1>
         
@@ -101,31 +111,34 @@ function Home(){
             <input type='submit' value='go' /><br/>
           </div>
         </form>
-        <div id='e_table_container' style={{display : 'inline-block'}}>
+        <div id='e_table_container'>
           <table id='e_info_table'>
             <thead>
               <tr className='table_head'>
-                <th><div className="t_div">E_NO</div></th>
-                <th><div className="t_div">NAME</div></th>
-                <th><div className="t_div">BIRTHDAY</div></th>
-                <th><div className="t_div">DEPTNO</div></th>
-                <th><div className="t_div">HOBBY</div></th>
-                <th><div className="t_div">PAY</div></th>
-                <th><div className="t_div">POSITION</div></th>
-                <th><div className="t_div">EDIT</div></th>
+                <th className='t_head'><div className="t_div">E_NO</div></th>
+                <th className='t_head'><div className="t_div">NAME</div></th>
+                <th className='t_head'><div className="t_div">BIRTHDAY</div></th>
+                <th className='t_head'><div className="t_div">DEPTNO</div></th>
+                <th className='t_head'><div className="t_div">HOBBY</div></th>
+                <th className='t_head'><div className="t_div">PAY</div></th>
+                <th className='t_head'><div className="t_div">POSITION</div></th>
+                <th className='t_head'><div className="t_div">EDIT</div></th>
               </tr>
             </thead>
             <tbody>
               { emp&&emp.map((data, index) => (
                   <tr className='table_tuple'>
                     <td><div className="user_info_capsule">{data.EMPNO}</div></td>
-                    <td><div className="user_info_capsule">{data.ENAME}</div></td>
+                    <td><div className="user_info_capsule">{data.NAME}</div></td>
                     <td><div className="user_info_capsule">{data.BIRTHDAY}</div></td>
                     <td><div className="user_info_capsule">{data.DEPTNO}</div></td>
                     <td><div className="user_info_capsule">{data.HOBBY}</div></td>
                     <td><div className="user_info_capsule">{data.PAY}</div></td>
                     <td><div className="user_info_capsule">{data.POSITION}</div></td>
-                    <td><div className="user_info_capsule">*</div></td>
+                    <td><div className="user_info_capsule"><svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" 
+                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                      </svg></div></td>
                   </tr>
               ))}          
             </tbody>
